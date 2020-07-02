7731c76a1ba81b454895c10010c1cfcbb776fd06
jfspps <jfsapps@gmail.com>
2020-07-02 21:07:53 +0100

    initialised Spring Boot app with dependencies

--- dbscripts/NodetableDB.js
+++ dbscripts/NodetableDB.js
@@ -15,10 +15,10 @@
 
 const mysql = require('mysql2');
 const dbConnection = mysql.createPool({
-    host     : 'localhost',
-    user     : 'SRM_admin',        
-    password : 'admin02passWORD&3',    
-    database : 'SRM'      
+    host: 'localhost',
+    user: 'SRM_admin',
+    password: 'admin02passWORD&3',
+    database: 'SRM'
 }).promise();
 
 module.exports = dbConnection;
---
+++ src/main/java/com/srm/srm_spring/SrmSpringApplication.java
@@ -0,0 +1,13 @@
+package com.srm.srm_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrmSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrmSpringApplication.class, args);
+    }
+
+}
