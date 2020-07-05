b35dd09e5c81ab0172451357bc1f02f776963436
jfspps <jfsapps@gmail.com>
2020-07-05 22:12:28 +0100

    modified controllers to have injected services, added Model arguments and updated index.html's

--- srm_spring_web/src/main/java/com/srm/controllers/GuardianIndexController.java
+++ srm_spring_web/src/main/java/com/srm/controllers/GuardianIndexController.java
@@ -1,14 +1,27 @@
 package com.srm.controllers;
 
+import com.srm.model.services.GuardianService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/guardians"})
 @Controller
 public class GuardianIndexController {
 
+    private final GuardianService guardianService;
+
+    //constructor service injection; when GuardianIndexController is instantiated, it is injected with a one-time GuardianService
+    public GuardianIndexController(GuardianService guardianService) {
+        this.guardianService = guardianService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listGuardians() {
+    public String listGuardians(Model model) {
+        //model is handled by Spring
+
+        //inject model into controller object with attribute 'guardians'
+        model.addAttribute("guardians", guardianService.findAll());
         return "guardians/index";
     }
 }
--- srm_spring_web/src/main/java/com/srm/controllers/StudentIndexController.java
+++ srm_spring_web/src/main/java/com/srm/controllers/StudentIndexController.java
@@ -1,14 +1,27 @@
 package com.srm.controllers;
 
+import com.srm.model.services.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping({"/students"})
 @Controller
 public class StudentIndexController {
 
+    private final StudentService studentService;
+
+    //constructor service injection; when StudentIndexController is instantiated, it is injected with a one-time StudentService
+    public StudentIndexController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listStudents() {
+    public String listStudents(Model model) {
+        //model is handled by Spring
+
+        //inject model into controller object with attribute 'students'
+        model.addAttribute("students", studentService.findAll());
         return "students/index";
     }
 }
